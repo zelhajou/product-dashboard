0b976aa6660eef29a7bd533cf3d5b826e237ae28
Zakaria Elhajoui <zakaria.elhajoui1@gmail.com>
2025-05-24 03:23:18 +0100

    refactor: remove ProductCard component and update exports in Product index

--- src/components/product/index.ts
+++ src/components/product/index.ts
@@ -1,4 +1,3 @@
-export { ProductCard } from './ProductCard';
 export { ProductFilters } from './ProductFilters';
 export { ProductForm } from './ProductForm';
 export { ProductTable } from './ProductTable';
